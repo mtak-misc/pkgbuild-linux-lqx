fbe43b305ef2005eb816bf33edfa44691065c385
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-01-19 12:58:45 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -6,12 +6,12 @@ pacman -Syu --noconfirm base-devel sudo schedtool jq unzip python
 # pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool python clang lld
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-#curl -sLJO -H 'Accept: application/octet-stream' \
-#"https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/assets/$( \
-#curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
-#| jq '.assets[] | select(.name | contains("llvm")) | .id')"
-#unzip llvm.zip
-#pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
+curl -sLJO -H 'Accept: application/octet-stream' \
+"https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/assets/$( \
+curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
+| jq '.assets[] | select(.name | contains("llvm")) | .id')"
+unzip llvm.zip
+pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 
 su builder -c "gpg --recv 38DBBDC86092693E"
 cd ./linux-lqx ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
