faf9d4d092ca7a56c96228a015b52ecf89b49d9d
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-13 10:13:44 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -6,11 +6,11 @@ pacman -Syu --noconfirm base-devel sudo schedtool jq unzip python
 # pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool python clang lld
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-# curl -sLJO -H 'Accept: application/octet-stream' \
-# "https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/assets/$( \
-# curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
-# | jq '.assets[] | select(.name | contains("llvm")) | .id')"
-# unzip llvm.zip
+curl -sLJO -H 'Accept: application/octet-stream' \
+"https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/assets/$( \
+curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
+| jq '.assets[] | select(.name | contains("llvm")) | .id')"
+unzip llvm.zip
 curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm/download/ -o llvm-x86_64.pkg.tar.zst
 curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm-libs/download/ -o llvm-libs-x86_64.pkg.tar.zst
 curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/lld/download/ -o lld-x86_64.pkg.tar.zst
