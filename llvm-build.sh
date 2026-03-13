e12ba5fa47a2955e51b53edc2c8d64eb5e9306f0
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-13 10:14:42 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -11,11 +11,6 @@ curl -sLJO -H 'Accept: application/octet-stream' \
 curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
 | jq '.assets[] | select(.name | contains("llvm")) | .id')"
 unzip llvm.zip
-curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm/download/ -o llvm-x86_64.pkg.tar.zst
-curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm-libs/download/ -o llvm-libs-x86_64.pkg.tar.zst
-curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/lld/download/ -o lld-x86_64.pkg.tar.zst
-curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/compiler-rt/download/ -o compiler-rt-x86_64.pkg.tar.zst
-curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/clang/download/ -o clang-x86_64.pkg.tar.zst
 
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 
