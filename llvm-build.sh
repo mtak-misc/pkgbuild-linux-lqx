fd8eaf7f66eca5e6f3ae1d14bf18deece5448baf
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-30 06:55:45 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 USERID=$1
 
-pacman -Syu --noconfirm base-devel sudo schedtool jq unzip python
+pacman -Syu --noconfirm base-devel sudo schedtool unzip python
 # pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool python clang lld
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
