21003e143bc45d2b6e89ecf42762ccdcab54599d
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-29 16:19:18 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 USERID=$1
-# GITHUB_TOKEN=$2
 
 pacman -Syu --noconfirm base-devel sudo schedtool jq unzip python
 # pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool python clang lld
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-#curl -sLJO -H 'Accept: application/octet-stream' \
-#"https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/assets/$( \
-#curl -sL https://${GITHUB_TOKEN}@api.github.com/repos/mtak-misc/archive/releases/tags/latest \
-#| jq '.assets[] | select(.name | contains("llvm")) | .id')"
-#unzip llvm.zip
 
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 
