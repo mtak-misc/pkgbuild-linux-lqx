3d17fee34765154b30b33badda3b72e5cb624662
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-05-08 07:01:51 +0900

    Update llvm-build.sh

--- llvm-build.sh
+++ llvm-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 USERID=$1
 
-pacman -Syu --noconfirm base-devel sudo schedtool unzip python
+pacman -Syu --noconfirm base-devel sudo schedtool python
 # pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool python clang lld
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
+# pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 
 su builder -c "gpg --recv 38DBBDC86092693E"
 cd ./linux-lqx ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
